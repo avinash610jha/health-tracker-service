5f69ab5ba316f29a42fb98b50b34bcee1b69def2
avinash610jha <avinash610jha@gmail.com>
2022-06-22 12:10:08 +0000

    updated the utility

--- srv/src/main/java/customer/health_tracker_service/rest/Utility.java
+++ srv/src/main/java/customer/health_tracker_service/rest/Utility.java
@@ -6,10 +6,23 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping(path = "/UtilityService")
 public class Utility {
 
-    @GetMapping(path = "/test")
-    public String testing(){
-        return "finally working with local authorization!!";
+    @GetMapping(path = "/testBASUrlOnEdge")
+    public String testingBAS(){
+        return "it's working on edge and copy it as curl and it works on postman as well!!";
     }
+
+    @GetMapping(path = "/testBASUrlOnPostman")
+    public String testingBASWithPostman(){
+        return "it's working when copy the request as curl and import in postman!!";
+    }
+
+    @GetMapping(path = "/testLocalHost")
+    public String testingLocal(){
+        return "it's working well when hosted locally with user and password avi ";
+
+    }
+
+
 
     
 
